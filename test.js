6b1c1806a66ef74fc3cb2af4a13280cc7a4423fb
frankiebee <frankie.diamond@gmail.com>
2017-06-06 21:20:16 -0700

    create a node polyfill

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const test = require('tape')
 const DB = require('./index.js')
-test('indexDb', function(t){
+test('indexDb#1', function(t){
     t.plan(2)
     const db = new DB({key: 'test', initialState: {test: 42}})
     db.open()
@@ -11,4 +11,16 @@ test('indexDb', function(t){
     .then(() => db.get())
     .then((data) => t.equal(data.test, 43, 'data should have changed'))
     .then(() => t.end())
+    .catch(console.error)
+})
+
+test('indexDb#2', function(t){
+    t.plan(1)
+    const db = new DB({key: 'test', initialState: {test: 42}})
+    db.open()
+    .then((data) => {
+      t.equal(data.test, 43, 'open should return the persisted data from before')
+    })
+    .then(() => t.end())
+    .catch(console.error)
 })
